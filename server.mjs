16bbde48b35c72ffc775bfda34b911196507b284
stphnrosa <stphn.rosa@gmail.com>
2025-04-22 23:06:29 -0400

    added dependencies

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT= process.env.PORT || 3000
+const app= express();
+
+
+
+
+
+app.listen(PORT, () => {
+    Console.log(`Server is running on port: ${PORT}`)
+});
